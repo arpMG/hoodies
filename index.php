80c2e43678b9890c75149719ae7e3d2ba08d063e
Andrew Pate <arp@mentonegrammar.net>
2020-03-06 12:21:36 +1100

    update

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MG Hoodies</title>
+</head>
+<body>
+    <div class="container">
+        <h1>MG Hoodies</h1>
+        <ul>
+            <li>Show all Orders</li>
+            <li>Show orders yet to be paid</li>
+            <li></li>
+        </ul>
+    </div>
+</body>
+</html>
