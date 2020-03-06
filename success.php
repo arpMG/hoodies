c300e7640a9687110ef6fe0ae8bd9f79ea1bb84c
Andrew Pate <arp@mentonegrammar.net>
2020-03-06 12:42:38 +1100

    a

---
+++ success.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MG Hoodies</title>
+</head>
+<body>
+    <div class="container">
+        <h1>MG Hoodies</h1>
+        <h2>Success</h2>
+        <p>Thanks, your order has been placed.</p>
+        <p>We will contact you if there are any concerns.</p>
+        <p>Payment is due to the office by Friday 13th March, 2020</p>
+    </div>
+    
+</body>
+</html>
